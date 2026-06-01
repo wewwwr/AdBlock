1f610b347296f6217ec91f071a58e11a05abc948
wewwwr <72biggest.syrups@icloud.com>
2026-06-01 13:31:45 +0300

    Add additional source URLs for domain lists

--- update_list.py
+++ update_list.py
@@ -16,6 +16,8 @@ SOURCE_URLS = [
     "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Shadowrocket/Advertising/Advertising_Domain.list",
     "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Shadowrocket/Advertising/Advertising.list",
     "https://dl.oisd.nl/oisd_big_surge.list",
+    "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Shadowrocket/Privacy/Privacy.list",
+    "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Shadowrocket/Hijacking/Hijacking.list",
     "https://badmojr.github.io/1Hosts/Lite/domains.txt",
     "https://raw.githubusercontent.com/misha-tgshv/shadowrocket-configuration-file/refs/heads/main/rules/domains_geo_detect.list",
     "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/domains/ultimate.txt",
